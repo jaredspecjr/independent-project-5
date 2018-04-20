fe3732c6a56b91682badc2dca3a14e3c8393fe70
jared <jaredspecjr@gmail.com>
2018-04-20 16:35:59 -0700

    add front end and finish html

--- src/main.js
+++ src/main.js
@@ -5,5 +5,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function(){
-
+  $("#form1").submit(function(event){
+    event.preventDefault();
+    let userBirthDate = new Calculator();
+    userBirthDate.age = $("#birthDate").val();
+    if($("#planet").val() === "mercury"){
+      userBirthDate.ageOnMercury();
+      $(".answer").text(userBirthDate.newAge);
+    } else if($("#planet").val() === "venus"){
+      userBirthDate.ageOnVenus();
+      $(".answer").text(userBirthDate.newAge);
+    } else if($("#planet").val() === "mars"){
+      userBirthDate.ageOnMars();
+      $(".answer").text(userBirthDate.newAge);
+    } else if($("#planet").val() === "jupiter"){
+      userBirthDate.ageOnJupiter();
+      $(".answer").text(userBirthDate.newAge);
+    }
+  });
 });
