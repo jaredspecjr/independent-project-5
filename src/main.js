de1ebaf89b7bc8e9186fed0f2d7403469216ff46
jared <jaredspecjr@gmail.com>
2018-04-20 15:46:55 -0700

    add simple html with input and select fields

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
+import { Calculator } from './../src/calculator.js'
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+$(document).ready(function(){
+
+});
