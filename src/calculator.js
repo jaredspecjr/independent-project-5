80ab6a3922064fe7d36859cc57bc602af347262c
jared <jaredspecjr@gmail.com>
2018-04-20 11:08:14 -0700

    add new method to find the difference of 2 dates

--- src/calculator.js
+++ src/calculator.js
@@ -1,10 +1,17 @@
 class Calculator{
-  constructor(age){
+  constructor(age, date){
     this.age = age;
+    this.date = date;
   }
   ageToSeconds(){
     let result = this.age * 31557600;
     return result;
+  }
+  datesToAge(){
+    let currentDate = new Date();
+    let birthDate = this.date;
+    let currentResult = currentDate.getFullYear();
+    let result = currentResult - birthDate;
   }
 }
 
